212b3639a410401cf3df12fa8000a3db0383e906
Benny <benny.sweetson@zenda.com>
2025-12-03 15:18:18 +0530

    bugfix/support-download-base64-data-url

--- src/ios/CDVWKInAppBrowser.m
+++ src/ios/CDVWKInAppBrowser.m
@@ -513,7 +513,7 @@ static CDVWKInAppBrowser* instance = nil;
         NSRange commaIndex = [urlString rangeOfString:@","];
         if (commaIndex.location != NSNotFound) {
             NSString *base64 = [urlString substringFromIndex:commaIndex.location + 1];
-            [self presentIOSShareSheetWithBase64:base64 filename:@"VietQR.png"];
+            [self presentIOSShareSheetWithBase64:base64 urlString:urlString];
         }
 
         // Cancel WKWebView navigation
@@ -556,16 +556,60 @@ static CDVWKInAppBrowser* instance = nil;
     }
 }
 
-// Custom: Intercept Base64 image download attempts
-- (void)presentIOSShareSheetWithBase64:(NSString *)base64 filename:(NSString *)filename {
+// Custom: Fallback filename generator based on MIME type
+- (NSString *)fallbackFileNameForMimeType:(NSString *)mimeType {
 
-    NSData *data = [[NSData alloc] initWithBase64EncodedString:base64
-                                                       options:NSDataBase64DecodingIgnoreUnknownCharacters];
+    // Try to mimic URLUtil.guessFileName fallback
+    NSString *fileName = nil;
+
+    // Fallback in case guess cannot determine name
+    NSString *timestamp = [NSString stringWithFormat:@"%lld",
+        (long long)([[NSDate date] timeIntervalSince1970] * 1000)];
+
+    fileName = [@"file_" stringByAppendingString:timestamp];
+
+    if ([mimeType containsString:@"png"]) {
+        fileName = [fileName stringByAppendingString:@".png"];
+    }
+    else if ([mimeType containsString:@"jpeg"] || [mimeType containsString:@"jpg"]) {
+        fileName = [fileName stringByAppendingString:@".jpg"];
+    }
+    else if ([mimeType containsString:@"pdf"]) {
+        fileName = [fileName stringByAppendingString:@".pdf"];
+    }
+    else {
+        fileName = [fileName stringByAppendingString:@".bin"];
+    }
+
+    return fileName;
+}
+
+// Custom: Intercept Base64 image download attempts
+- (void)presentIOSShareSheetWithBase64:(NSString *)base64 urlString:(NSString *)urlString {
+
+    NSData *data = [[NSData alloc] initWithBase64EncodedString:base64 options:NSDataBase64DecodingIgnoreUnknownCharacters];
 
     if (!data) return;
 
+    // Guess fileNmae
+    NSString *mimeType = nil;
+
+    // Extract MIME type from data URL: data:image/png;base64,...
+    NSString *urlString = url.absoluteString;
+    NSRange mimeStart = [urlString rangeOfString:@"data:"];
+    NSRange mimeEnd = [urlString rangeOfString:@";base64"];
+    if (mimeStart.location != NSNotFound && mimeEnd.location != NSNotFound) {
+        mimeType = [urlString substringWithRange:NSMakeRange(mimeStart.location + 5,
+                        mimeEnd.location - (mimeStart.location + 5))];
+    }
+    // Use Android-style fallback if MIME is missing
+    if (mimeType == nil) {
+        mimeType = @"application/octet-stream";
+    }
+    NSString *fileName = [self fallbackFileNameForMimeType:mimeType];
+
     // Create a temporary file for sharing
-    NSString *tempPath = [NSTemporaryDirectory() stringByAppendingPathComponent:filename];
+    NSString *tempPath = [NSTemporaryDirectory() stringByAppendingPathComponent:fileName];
     NSURL *tempURL = [NSURL fileURLWithPath:tempPath];
 
     // Write file
